e5dbc9bb37520abaefbcbac0e0eaa69d236e94a6
刘圣伟 <1064084779@qq.com>
2022-05-13 22:10:06 +0800

    feat: make a tag

--- react18/src/index.js
+++ react18/src/index.js
@@ -1,17 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+let instance = null;
+
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+const render = () => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+};
+if (!window.__MICRO_WEB__) {
+  render();
+}
+export const bootstrap = () => {
+  console.log("开始加载");
+};
+export const mount = () => {
+  render();
+  console.log("渲染成功");
+};
+export const unmount = () => {
+  console.log("卸载", instance);
+};
